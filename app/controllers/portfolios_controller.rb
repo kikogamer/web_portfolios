fa9f0cd5738477852378d86eaa0317c1b61ea42d
Ronaldo <kikogamer@gmail.com>
2019-10-13 21:53:07 -0300

    feature portfolio create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,15 @@ class PortfoliosController < ApplicationController
       authorize(Portfolio)
       @portfolios = policy_scope(Portfolio)
   end 
+
+  def create
+    slug_generator = PortfolioSlugGeneratorService.new().call
+    @portfolio = current_user.portfolios.create(slug: slug_generator)
+    redirect_to edit_portfolio_path(@portfolio)
+  end
+
+  def edit
+    @portfolio = policy_scope(Portfolio).find(params[:id])
+    authorize @portfolio
+  end
 end
--- app/policies/portfolio_policy.rb
+++ app/policies/portfolio_policy.rb
@@ -1,8 +1,19 @@
 class PortfolioPolicy < ApplicationPolicy
+  
   def index?
     true
   end
 
+  def create?
+    record.user == user
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  private 
+  
   class Scope < Scope
     def resolve
       user.portfolios
