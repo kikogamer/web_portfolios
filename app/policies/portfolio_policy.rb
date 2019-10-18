9f61957ab3a3aba52db2d80dab34f018426df45c
Ronaldo <kikogamer@gmail.com>
2019-10-17 23:45:23 -0300

    portfolio resources

--- app/policies/portfolio_policy.rb
+++ app/policies/portfolio_policy.rb
@@ -1,29 +1,40 @@
 class PortfolioPolicy < ApplicationPolicy
-  
   def index?
     true
   end
 
   def create?
-    record.user == user
-  end
-
-  def edit?
-    record.user == user
+    permitted_action?
   end
 
   def show?
     true
   end
 
+  def edit?
+    permitted_action?
+  end
+
   def update?
+    edit?
+  end
+
+  def destroy?
+    permitted_action?
+  end
+
+  private
+
+  def permitted_action?
     record.user == user
   end
 
-  private 
-  
   class Scope < Scope
     def resolve
+      permitted_portfolios
+    end
+
+    def permitted_portfolios
       user.portfolios
     end
   end
