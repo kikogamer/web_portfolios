19459f6783201637407514a61220b3f5341b0fae
Ronaldo <kikogamer@gmail.com>
2019-10-20 12:09:32 -0300

    additional informatrion block feature

--- app/models/additional_information.rb
+++ app/models/additional_information.rb
@@ -1,5 +1,7 @@
 class AdditionalInformation < ApplicationRecord
+  belongs_to :block
   belongs_to :portfolio
-
+  
+  validates :block_id, uniqueness: true
   validates :title, :description, presence: true
 end
