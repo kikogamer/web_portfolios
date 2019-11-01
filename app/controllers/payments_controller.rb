a930e5accd2dfed482b1590230155396304eddd3
Ronaldo <kikogamer@gmail.com>
2019-10-31 23:50:06 -0300

    creating order

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,6 +5,12 @@ class PaymentsController < ApplicationController
     @order = @portfolio.order
   end
 
+  def create
+    @order = MoipCreateOrderService.new(@portfolio, params).call
+    MoipCreatePaymentService.new(@order, params).call
+    redirect_to "/portfolios/#{params[:portfolio_id]}/payments"
+  end
+
   private
 
     def set_portfolio
