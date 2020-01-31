9bc35257e96bfced03ceb5b1ccced6935d8b8c5d
ronaldo <kikogamer@gmail.com>
2020-01-30 22:38:55 -0300

    project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# projects controller
+class ProjectsController < PortfolioResourcesController
+  protected
+
+  def load_portfolio_resources
+    @portfolio.projects
+  end
+
+  def load_portfolio_resource
+    @portfolio.projects.find(params[:id])
+  end
+
+  def build_portfolio_resource
+    @portfolio.projects.build
+  end
+
+  def portfolio_resource_params
+    params.require(:project).permit(:title, :description, :kind, :link)
+  end
+end
