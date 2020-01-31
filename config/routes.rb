cae610fb8a86fb376904051da8cc0d3161063b5a
ronaldo <kikogamer@gmail.com>
2020-01-30 22:41:25 -0300

    project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'payments/index'
   get 'payments/create'
@@ -5,28 +7,29 @@ Rails.application.routes.draw do
   get 'contact_forms/create'
   root 'pages#home'
   devise_for :users
-  get :search, to: "search#index"
-  
+  get :search, to: 'search#index'
+
   post '/webhooks', to: 'webhooks#webhooks'
-  
+
   resources :portfolios, except: :new do
     member do
-      resources :payments, only: [:index, :create]
+      resources :payments, only: %i[index create]
     end
-    resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
-    resources :blocks, only: [:index, :create, :destroy] do
-      resources :additional_informations, only: [:index, :update]
-      patch :positions, on: :collection, to: "block_positions#update"
+    resources :tags, only: %i[create destroy], param: :tag_id, controller: :portfolio_tags
+    resources :blocks, only: %i[index create destroy] do
+      resources :additional_informations, only: %i[index update]
+      patch :positions, on: :collection, to: 'block_positions#update'
     end
-    resources :profiles, only: [:index, :update]
-    resources :experiences, only: [:index, :create, :destroy]
-    resources :educations, only: [:index, :create, :destroy]
-    resources :skills, only: [:index, :create, :destroy]
-    resources :hobbies, only: [:index, :create, :destroy]
-    resources :languages, only: [:index, :create, :destroy]
-    resources :abouts, only: [:index, :update]
-    resources :socials, only: [:index, :create, :destroy]
-    resources :contacts, only: [:index, :create, :update]
+    resources :profiles, only: %i[index update]
+    resources :experiences, only: %i[index create destroy]
+    resources :educations, only: %i[index create destroy]
+    resources :skills, only: %i[index create destroy]
+    resources :hobbies, only: %i[index create destroy]
+    resources :languages, only: %i[index create destroy]
+    resources :abouts, only: %i[index update]
+    resources :socials, only: %i[index create destroy]
+    resources :contacts, only: %i[index create update]
+    resources :projects, only: %i[index create update]
   end
 
   resources :contact_forms, only: [:create]
